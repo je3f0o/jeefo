bc9954ae700697f8907de8264d3c3eaf3f52f49b
je3f0o <je3f0o@gmail.com>
2021-01-14 02:40:46 +0800

    version: 0.2.6

--- src/jeefo/certificate_manager.js
+++ src/jeefo/certificate_manager.js
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name   : certificate_manager.js
 * Created at  : 2021-01-11
-* Updated at  : 2021-01-11
+* Updated at  : 2021-01-14
 * Author      : jeefo
 * Purpose     :
 * Description :
@@ -16,7 +16,10 @@
 // ignore:end
 
 const fs       = require("@jeefo/fs");
+const path     = require("path");
 const Readonly = require("@jeefo/utils/object/readonly");
+
+const {root_dir} = require("../server/paths");
 
 class JeefoCertificate {
     constructor (key, certificate) {
@@ -36,10 +39,17 @@ class JeefoCertificateManager {
 
         readonly.prop("initialize", async hostname => {
             if (is_initialized) throw new Error("Already initialized");
-            if (! hostname) hostname = "localhost";
 
-            const rsa = `./certs/${hostname}/rsa.key`;
-            const ssc = `./certs/${hostname}/self_signed_certificate`;
+            let certs_dir;
+            if (! hostname) {
+                hostname  = "localhost";
+                certs_dir = path.normalize(`${__dirname}/../certs`);
+            } else {
+                certs_dir = `${root_dir}/certs`;
+            }
+
+            const rsa = `${certs_dir}/${hostname}/rsa.key`;
+            const ssc = `${certs_dir}/${hostname}/self_signed_certificate`;
             const key  = await fs.readFile(rsa, "utf8");
             const cert = await fs.readFile(ssc, "utf8");
             certificate = new JeefoCertificate(key, cert);
